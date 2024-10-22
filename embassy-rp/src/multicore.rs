fbe09be05a67572b06eb934c7e0a938d8e1fa5bd
Elena Frank <elena.frank@protonmail.com>
2024-10-22 09:50:02 +0200

    embassy_rp: fix unsafe call to extern fn

--- embassy-rp/src/multicore.rs
+++ embassy-rp/src/multicore.rs
@@ -295,7 +295,9 @@ pub fn pause_core1() {
                     break;
                 }
                 #[cfg(feature = "fifo-handler")]
-                handle_fifo_token(token);
+                unsafe {
+                    handle_fifo_token(token);
+                }
             }
         })
     }
@@ -313,7 +315,9 @@ pub fn resume_core1() {
                     break;
                 }
                 #[cfg(feature = "fifo-handler")]
-                handle_fifo_token(token);
+                unsafe {
+                    handle_fifo_token(token);
+                }
             }
         })
     }
